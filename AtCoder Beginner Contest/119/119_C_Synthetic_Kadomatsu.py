f643cc8611201d8f69b4b6b1510a754054b1bf42
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 11:32:09 +0900

    Update 119_C_Synthetic_Kadomatsu.py

--- AtCoder Beginner Contest/119/119_C_Synthetic_Kadomatsu.py
+++ AtCoder Beginner Contest/119/119_C_Synthetic_Kadomatsu.py
@@ -1,16 +1,13 @@
 N,A,B,C = map(int,input().split())
 l = [int(input()) for i in range(N)]
-INF = 10**9
 
 def dfs(cur,a,b,c):
     if cur == N:
-        return abs(a-A) + abs(b-B) + abs(c-C) - 30 if min(a,b,c) > 0 else INF
-    print('l'+str(cur)+':'+str(l[cur]))
-    print('cur:'+str(cur),'a:'+str(a),'b:'+str(b),'c:'+str(c))
+        return abs(a-A) + abs(b-B) + abs(c-C) - 30 if min(a,b,c) > 0 else 10**9
     ret0 = dfs(cur+1,a,b,c)
-    ret1 = dfs(cur+1,a+l[cur],b,c) + 10
-    ret2 = dfs(cur+1,a,b+l[cur],c) + 10
-    ret3 = dfs(cur+1,a,b,c+l[cur]) + 10
+    ret1 = dfs(cur+1,a+l[cur],b,c) + (10 if a != 0 else 0)
+    ret2 = dfs(cur+1,a,b+l[cur],c) + (10 if b != 0 else 0)
+    ret3 = dfs(cur+1,a,b,c+l[cur]) + (10 if c != 0 else 0)
     return min(ret0,ret1,ret2,ret3)
 
 print(dfs(0,0,0,0))
