ddd66cd45653e93736b8f21cf3e66ab05596cb28
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 12:11:15 +0900

    Update 112_A_Programming_Education.py

--- AtCoder Beginner Contest/112/112_A_Programming_Education.py
+++ AtCoder Beginner Contest/112/112_A_Programming_Education.py
@@ -1,25 +1,8 @@
-N = 2
-A = 3
-B = 5
-
 N = int(input())
-try:
-    A = int(input())
-    B = int(input())
-except:
-    ''
 
 if N == 1:
     print('Hello World')
 elif N == 2:
+    A = int(input())
+    B = int(input())
     print(A+B)
-
-#解答2
-# N = int(input())
-#
-# if N == 1:
-#     print('Hello World')
-# elif N == 2:
-#     A = int(input())
-#     B = int(input())
-#     print(A+B)
