be33e09519e972f5d37e93c72ce4e32c313d2e2b
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 10:46:24 +0900

    Update 123_B_Five_Dishes_2.py

--- AtCoder Beginner Contest/123/123_B_Five_Dishes_2.py
+++ AtCoder Beginner Contest/123/123_B_Five_Dishes_2.py
@@ -1,1 +1,18 @@
+import itertools
+list = [int(input()) for _ in range(5)]
+temp = 0
+ans = 10**9
 
+for v in itertools.permutations(list):
+    temp = 0
+    for i,j in enumerate(v):
+        if i != 4:
+            if j % 10 == 0:
+                temp += j
+            else:
+                temp += (j//10+1)*10
+        else:
+            temp += j
+    ans = min(ans,temp)
+
+print(ans)
