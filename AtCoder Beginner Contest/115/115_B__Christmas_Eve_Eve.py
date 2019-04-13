d522b20bf1a1d797630d8d9abd9fe78dbc98f9a8
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 11:44:08 +0900

    Update 115_B__Christmas_Eve_Eve.py

--- AtCoder Beginner Contest/115/115_B__Christmas_Eve_Eve.py
+++ AtCoder Beginner Contest/115/115_B__Christmas_Eve_Eve.py
@@ -5,15 +5,3 @@ list.sort(reverse=True)
 total = list[0]/2 + sum(list[1:])
 
 print(int(total))
-
-#第一回提出
-# N = int(input())
-# list = [int(input()) for i in range(N)]
-# list.sort(reverse=True)
-#
-# total = list[0]/2
-#
-# for i in list[1:]:
-#     total += i
-#
-# print(int(total))
