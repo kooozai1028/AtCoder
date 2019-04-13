e40add4679c507147f9bb6f75e75f19fa100c0a8
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 11:41:27 +0900

    Update 116_B_Collatz_Problem.py

--- AtCoder Beginner Contest/116/116_B_Collatz_Problem.py
+++ AtCoder Beginner Contest/116/116_B_Collatz_Problem.py
@@ -10,21 +10,3 @@ while s not in list:
         s = 3*s + 1
 
 print(len(list)+1)
-
-#初回提出
-# s = int(input())
-#
-# cnt = 1
-# list = []
-# list.append(s)
-# while True:
-#     if s % 2 == 0:
-#         s = int(s/2)
-#     else:
-#         s = 3*s + 1
-#     cnt += 1
-#     if s in list:
-#         break
-#     list.append(s)
-#
-# print(cnt)
