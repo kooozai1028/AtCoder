ad82d5cfe09d21e087fb3ebc9c9820bc87b0c83b
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 11:44:33 +0900

    Update 115_C_Christmas_Eve.py

--- AtCoder Beginner Contest/115/115_C_Christmas_Eve.py
+++ AtCoder Beginner Contest/115/115_C_Christmas_Eve.py
@@ -4,14 +4,3 @@ list = [int(input()) for i in range(N)]
 list.sort()
 
 print(min(list[i+K-1]-list[i] for i in range(N-K+1)))
-
-#第一回提出
-# N,K = map(int,input().split())
-#
-# list = [int(input()) for i in range(N)]
-# list.sort()
-# min_val = 10**9
-# for i in range(N-K+1):
-#     min_val = min(min_val,list[i+K-1]-list[i])
-#
-# print(min_val)
