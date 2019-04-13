3a04e00a08d48e486fb43ee110c064e79ea50f0f
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 12:14:19 +0900

    Update 113_B_Palace.py

--- AtCoder Beginner Contest/113/113_B_Palace.py
+++ AtCoder Beginner Contest/113/113_B_Palace.py
@@ -7,19 +7,3 @@ T,A = map(int,input().split())
 H = list(map(int,input().split()))
 
 H_temp = [abs(A-(T-0.006*i)) for i in H]
-
-#解答１　解が一意に定まる前提で最小値を探索
-for i in range(len(H_temp)):
-    if H_temp[i] == min(H_temp):
-        print(i+1)
-
-#解答２　最小値書き換え
-# temp = 999999
-# best_spot = None
-#
-# for i in range(len(H_temp)):
-#     if H_temp[i] < temp:
-#         temp = H_temp[i]
-#         best_spot = i+1
-#
-# print(best_spot)
