17aee70de28c289a7f519b47e2c0b2f58874c4aa
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 11:34:16 +0900

    Update and rename 118_B_Foods_Loved_by_Everyone.py to 118_B_Foods_Loved_by_Everyone_set.py

---
+++ AtCoder Beginner Contest/118/118_B_Foods_Loved_by_Everyone_set.py
@@ -0,0 +1,13 @@
+#質問の全量をリストとして格納
+#各行に質問リストの項目が存在するか確認し、あれば残す
+#最終的に残っていた項目が解答
+
+N,M = map(int,input().split())
+res = set(range(1,M+1))
+print(res)
+
+for i in range(N):
+    a,*b = map(int,input().split())
+    res &= set(b)
+
+print(len(res))
