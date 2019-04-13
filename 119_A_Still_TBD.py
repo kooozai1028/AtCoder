ed3c6aaf935e07d60927e88597b9567496a50602
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 10:29:53 +0900

    Delete 119_A_Still_TBD.py

--- 119_A_Still_TBD.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-date = datetime.datetime.strptime(input(),'%Y/%m/%d')
-cri_date = datetime.datetime(2019,4,30)
-
-if date <= cri_date:
-    print('Heisei')
-else:
-    print('TBD')
