a6e706dcec4f8873fa89210d748855ff658f1868
kooozai1028 <kooozai1028@gmail.com>
2019-04-13 12:03:40 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/111/111_A_AtCoder999.py
@@ -0,0 +1,13 @@
+# n = [x for x in str(1199)]
+n = [x for x in str(input())]
+add_stock = ''
+
+for i in n:
+    add = i
+    if i == '1':
+        add = '9'
+    elif i == '9':
+        add = '1'
+    add_stock += add
+
+print(add_stock)
