befbdb776172494df556b1136b8eb135d3e277c4
kooozai1028 <kooozai1028@gmail.com>
2019-04-20 14:34:09 +0900

    Update 119_C_Synthetic_Kadomatsu.py

--- AtCoder Beginner Contest/119/119_C_Synthetic_Kadomatsu.py
+++ AtCoder Beginner Contest/119/119_C_Synthetic_Kadomatsu.py
@@ -1,13 +1,13 @@
-N,A,B,C = map(int,input().split())
-l = [int(input()) for i in range(N)]
-
-def dfs(cur,a,b,c):
-    if cur == N:
-        return abs(a-A) + abs(b-B) + abs(c-C) - 30 if min(a,b,c) > 0 else 10**9
-    ret0 = dfs(cur+1,a,b,c)
-    ret1 = dfs(cur+1,a+l[cur],b,c) + (10 if a != 0 else 0)
-    ret2 = dfs(cur+1,a,b+l[cur],c) + (10 if b != 0 else 0)
-    ret3 = dfs(cur+1,a,b,c+l[cur]) + (10 if c != 0 else 0)
-    return min(ret0,ret1,ret2,ret3)
-
+N,A,B,C = list(map(int,input().split()))
+list = [int(input()) for _ in range(N)]
+ 
+def dfs(cnt,a,b,c):
+    if cnt == N:
+        return abs(A-a)+abs(B-b)+abs(C-c) if min(a,b,c) > 0 else float('inf')
+    l0 = dfs(cnt+1,a,b,c)
+    la = dfs(cnt+1,a+list[cnt],b,c) +(10 if a != 0 else 0)
+    lb = dfs(cnt+1,a,b+list[cnt],c) +(10 if b != 0 else 0)
+    lc = dfs(cnt+1,a,b,c+list[cnt]) +(10 if c != 0 else 0)
+    return min(l0,la,lb,lc)
+ 
 print(dfs(0,0,0,0))
