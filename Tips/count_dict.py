e69f5cf97838e62a2d228f474e337e9302861107
kooozai1028 <kooozai1028@gmail.com>
2019-05-01 20:12:34 +0900

    Update count_dict.py

--- Tips/count_dict.py
+++ Tips/count_dict.py
@@ -1,8 +1,12 @@
-def cnt_element(x):
+S = input()
+
+def cnt_element(X):
     dict = {}
-    for i in x:
-        if i not in dict:
-            dict[i] = 1
+    for x in X:
+        if x not in dict:
+            dict[x] = 1
         else:
-            dict[i] += 1
-    return  sorted([x for x in dict.values()],reverse=True)
+            dict[x] += 1
+    return sorted([x for x in dict.items()],reverse=True,key=lambda x:x[1])
+
+print(cnt_element(S))
